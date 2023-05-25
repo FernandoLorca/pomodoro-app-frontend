a5f7b647c982c26710e482a7c2e55339f5d49993
FernandoLorca <florcapinto@gmail.com>
2023-05-24 22:37:25 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <button>Pomodoro</button>
+        </li>
+        <li>
+          <button>Short Break</button>
+        </li>
+        <li>
+          <button>Long Break</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
