723bf81a7c2d56201249a8ec8f611821a735227b
FernandoLorca <florcapinto@gmail.com>
2023-05-26 10:37:11 -0400

    automatically reorder the tailwind classes thantks to the plugin

--- src/components/CountDownTimer/ButtonStart.jsx
+++ src/components/CountDownTimer/ButtonStart.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ButtonStart = ({ startTimer, text, isRunning }) => {
   return (
     <button
-      className={`flex items-center bg-blue-500 buttonTimer text-white px-16 py-4 rounded-2xl transition-transform hover:-translate-y-2 active:translate-y-0 ${
+      className={`buttonTimer flex items-center rounded-2xl bg-blue-500 px-16 py-4 text-white transition-transform hover:-translate-y-2 active:translate-y-0 ${
         isRunning && 'translate-y-0 hover:translate-y-0'
       }`}
       onClick={startTimer}
