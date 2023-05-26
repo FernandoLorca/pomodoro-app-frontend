5ce38f40113dd6a79974b02f4a7a4c9aa7d490a1
FernandoLorca <florcapinto@gmail.com>
2023-05-26 10:03:40 -0400

    edit the classname of buttonstart component, button now response to hover and active behavior and in result of that the button move in y axis

--- src/components/CountDownTimer/ButtonStart.jsx
+++ src/components/CountDownTimer/ButtonStart.jsx
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 const ButtonStart = ({ startTimer, text, isRunning }) => {
   return (
     <button
-      className="flex items-center bg-blue-500 buttonTimer text-white px-16 py-4 rounded-2xl"
+      className={`flex items-center bg-blue-500 buttonTimer text-white px-16 py-4 rounded-2xl transition-transform hover:-translate-y-2 active:translate-y-0 ${
+        isRunning && 'translate-y-0 hover:translate-y-0'
+      }`}
       onClick={startTimer}
     >
       <BsFillPlayFill className={`mr-1 ${isRunning ? 'hidden' : 'block'}`} />
