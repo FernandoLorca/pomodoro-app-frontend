035e34bbcb455fede1fff5ffef37039954f33298
FernandoLorca <florcapinto@gmail.com>
2023-05-26 15:10:51 -0400

    create the create browser on main.jsx file. And configure the first route that is RouterPomodoro.jsx in router folder

---
+++ src/router/RouterPomodoro.jsx
@@ -0,0 +1,11 @@
+import CountDownComponent from '../components/CountDownComponent/CountDownComponent';
+
+const RouterPomodoro = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <CountDownComponent />
+    </div>
+  );
+};
+
+export default RouterPomodoro;
